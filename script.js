3ea3edb496fc07d86a99baf48f5a1ef11c1183bc
unknown <aril.imata061103@gmail.com>
2023-09-14 19:35:03 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Sticky Header
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header-container");
+
+// Get the offset position of the navbar
+var fixedPosition = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.scrollY > fixedPosition) {
+      header.classList.add("fixed-position");
+    } else {
+      header.classList.remove("fixed-position");
+    }
+}
+
+//Get Date
+const date = new Date();
+const formattedDate = date.toLocaleDateString('en-GB', {day: 'numeric', month: 'long', year: 'numeric'}).replace(/ /g, ' ');
+document.getElementById("today-news-date").innerHTML = formattedDate;
